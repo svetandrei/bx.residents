1bd5f18f774573e92153c046dc09af92119aad81
Andrei <svetsandrew@gmail.com>
2021-10-15 21:15:10 +0300

    Task Residents

--- local/components/bitrix/residents.list/templates/.default/template.php
+++ local/components/bitrix/residents.list/templates/.default/template.php
@@ -1,10 +1,8 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {die();}?>
 <ul>
 <?php
-foreach($arResult["ITEMS"] as $keyItem => $arItem):
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));?>
-    <li class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+foreach($arResult["ITEMS"] as $keyItem => $arItem):?>
+    <li class="news-item" id="<?=$this->GetEditAreaId($keyItem);?>">
         <?=$arItem["FIO"];?> - <?=$arItem["IBLOCK_ELEMENTS_ELEMENT_HOUSES_CITY_VALUE"];?>,<?=$arItem["IBLOCK_ELEMENTS_ELEMENT_HOUSES_STREET_VALUE"];?>,<?=$arItem["IBLOCK_ELEMENTS_ELEMENT_HOUSES_NUMBER_VALUE"];?>
     </li>
 <?endforeach;?>
