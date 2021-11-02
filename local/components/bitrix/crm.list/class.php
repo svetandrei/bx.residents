1b37828716eb938183c45ef0f2daac487a58b20f
Andrei <svetsandrew@gmail.com>
2021-11-02 16:29:51 +0200

    Task CRM

--- local/components/bitrix/crm.list/class.php
+++ local/components/bitrix/crm.list/class.php
@@ -1,7 +1,9 @@
-<?
-use Bitrix\Main\Localization\Loc;
+<?php
 
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+use Bitrix\Main\Localization\Loc;
 
 class CRMListComponent extends CBitrixComponent
 {
@@ -10,58 +12,56 @@ class CRMListComponent extends CBitrixComponent
      */
     protected $arRes = [];
     protected $arSelect = [];
-	protected $userIDs = [];
+    protected $userIDs = [];
+/**
+     * @param array $userIDs
+     * @return array|void
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     * @throws \Bitrix\Main\SystemException
+     */
+    protected function getUsers(array $userIDs)
+    {
+        return \Bitrix\Main\UserTable::getList(array(
+            'select' => ['ID','NAME'],
+            'filter' => ['ID' => $userIDs],
+            'cache' => ['ttl' => $this->arParams['CACHE_TIME']],
+        ))->fetchAll();
+    }
 
-	/**
-	 * @param array $userIDs
-	 * @return array|void
-	 * @throws \Bitrix\Main\ArgumentException
-	 * @throws \Bitrix\Main\ObjectPropertyException
-	 * @throws \Bitrix\Main\SystemException
-	 */
-	protected function getUsers(array $userIDs)
-	{
-		return \Bitrix\Main\UserTable::getList(array(
-			'select' => ['ID','NAME'],
-			'filter' => ['ID' => $userIDs],
-			'cache' => ['ttl' => $this->arParams['CACHE_TIME']],
-		))->fetchAll();
-	}
-
-	/**
-	 * @param array $arCRM
-	 * @param int $returnName
-	 * @param array $users
-	 * @return array
-	 */
-	private function checkArr(array $arCRM, int $returnName, array $users)
-	{
-		$arr = [];
-		foreach ($arCRM as $keyString => $val)
-		{
-			if (preg_match('/CLIENT_VALUE|MANAGER_VALUE/', $keyString))
-			{
-				if ($returnName)
-				{
-					foreach ($users as $arUser)
-					{
-						if ((int)$val === (int)$arUser['ID'])
-						{
-							$arr[$keyString] = $arUser['NAME'];
-						}
-					}
-				} else {
-					$this->userIDs[] = (int)$val;
-					$arr[$keyString] = $val;
-				}
-			} elseif (preg_match('/CLIENT*|MANAGER*/', $keyString)) {
-				unset($arCRM[$keyString]);
-			} else {
-				$arr[$keyString] = $val;
-			}
-		}
-		return $arr;
-	}
+    /**
+     * @param array $arCRM
+     * @param int $returnName
+     * @param array $users
+     * @return array
+     */
+    private function checkArr(array $arCRM, int $returnName, array $users)
+    {
+        $arr = [];
+        foreach ($arCRM as $keyString => $val) {
+            switch ($keyString) {
+                case (bool)preg_match('/CLIENT_VALUE|MANAGER_VALUE/', $keyString):
+                    switch ($returnName) {
+                        case (bool)$returnName:
+                            foreach ($users as $arUser) {
+                                if ((int)$val === (int)$arUser['ID']) {
+                                    $arr[$keyString] = $arUser['NAME'];
+                                }
+                            }
+                        default:
+                            $this->userIDs[] = (int)$val;
+                            $arr[$keyString] = $val;
+                    }
+                    break;
+                case (bool)preg_match('/CLIENT*|MANAGER*/', $keyString) === true:
+                    unset($arCRM[$keyString]);
+                    break;
+                default:
+                    $arr[$keyString] = $val;
+            }
+        }
+        return $arr;
+    }
 
     /**
      *
@@ -71,7 +71,7 @@ class CRMListComponent extends CBitrixComponent
      * @throws \Bitrix\Main\SystemException
      */
     protected function getCRM(int $returnUserIds)
-	{
+    {
         $classCRM = \Bitrix\Iblock\Iblock::wakeUp($this->arParams['IBLOCK_ID'])->getEntityDataClass();
         $rsCRM = $classCRM::getList(array(
             'select' => ['ID', 'NAME', 'NUMBER.VALUE', 'PRICE.VALUE', 'CLIENT', 'MANAGER', 'STATUS.ITEM.VALUE'],
@@ -79,19 +79,16 @@ class CRMListComponent extends CBitrixComponent
             'order' => ['TIMESTAMP_X' => 'DESC'],
             'cache' => ['ttl' => $this->arParams['CACHE_TIME']],
         ));
-        foreach ($rsCRM->fetchAll() as $arCRM)
-		{
-			$this->arRes['ITEMS'][$arCRM['ID']] = $this->checkArr($arCRM, false, array());
+        foreach ($rsCRM->fetchAll() as $arCRM) {
+            $this->arRes['ITEMS'][$arCRM['ID']] = $this->checkArr($arCRM, false, array());
         }
-		$this->userIDs = array_unique($this->userIDs);
-		if ($returnUserIds)
-		{
-			return false;
-		}
-		foreach ($this->arRes['ITEMS'] as $key => $arCRM)
-		{
-			$this->arRes['ITEMS'][$key] = $this->checkArr($arCRM, true, $this->getUsers($this->userIDs));
-		}
+        $this->userIDs = array_unique($this->userIDs);
+        if ($returnUserIds) {
+            return false;
+        }
+        foreach ($this->arRes['ITEMS'] as $key => $arCRM) {
+            $this->arRes['ITEMS'][$key] = $this->checkArr($arCRM, true, $this->getUsers($this->userIDs));
+        }
         return $this->arRes;
     }
 
